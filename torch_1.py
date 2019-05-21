122bc8878a72e10e2ee56454e900622db820d869
sjukdom <svrt.hlvt@gmail.com>
2019-05-20 20:00:26 -0500

    rnn is almost done

--- torch_1.py
+++ torch_1.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 Corpus = [
    '<BOS> el puto perro me mordio la puta mano <EOS>',
@@ -15,8 +16,12 @@ s = [sentence.split() for sentence in Corpus]
 Sigma = []
 for sentence in s:
    for word in sentence:
-      if word not in Sigma:
-         Sigma.append(word)
+      if word != '<BOS>' and word != '<EOS>':
+         if word not in Sigma:
+            Sigma.append(word)
+
+Sigma.append('<EOS>')
+Sigma.append('<BOS>')
 
 print('\nAlfabeto:')
 print(Sigma)
@@ -37,106 +42,99 @@ print('\n')
 for sidx in sentIndexed:
    print(sidx)
 
-# Crear bigramas del corpus
-bigrams = []
-for sentence in sentIndexed:
-   for wi, wj in zip(sentence[:-1], sentence[1:]):
-      bigrams.append((wi, wj))
+# Crear bigramas del corpus 
+def getBigrams(sentences):
+   bigrams = []
+   for sentence in sentences:
+      bigrams.append([(wi, wj) for wi, wj in zip(sentence[:-1], sentence[1:])])
+   return bigrams
+
+bigrams = getBigrams(sentIndexed)
 
 print('\n')
-print(bigrams)
+for sentence in bigrams:
+   print(sentence)
 
-for pair in bigrams:
+# Imprimir bigramas de la primera oracion
+print('\n')
+for pair in bigrams[0]:
    print('({}, {})'.format(indx2word[pair[0]], indx2word[pair[1]]))
 
 
 # Red neuronal recurrente
-
-## Parametros de la red
-N = len(Sigma)
-d = 100
-rnnDim = 100
-
-## Embedding
-C = torch.randn(d, N)
-### Parametros capa 1
-Whh = torch.randn(rnnDim, rnnDim)
-Wxh = torch.randn(rnnDim, d)
-b = torch.randn(rnnDim)
-### Parametros capa 2
-W = torch.randn(N-1, rnnDim)
-c = torch.zeros(N-1)
-
-## Estado oculto
-ht = torch.zeros(5, 2)
-
-print('\nWhh')
-print(Whh)
-
-print('\nWxh')
-print(Wxh)
-
-print('\nV')
-print(V)
-
-print('\nht0')
-print(ht)
-
-def dTanh(xt):
-   return 1 - torch.tanh(xt)**2
-
-def forward(xt):
-   xt = E[:, xt]
-   at = torch.matmul(Whh, ht) + torch.matmul(Wxh, xt) + b
-   ht = torch.tanh(at)
-   ot = torch.matmul(V, ht) + c
-   yt = torch.exp(ot - torch.max(ot))/ torch.sum(ot - torch.max(ot))
-   return yt 
-
-def backprop(dt, lr, df):
-   gradL_a = torch.matmul(torch.diag(df()))
-   dV = torch.ger(dt, ht)
-   dc = dt
-   dWhh = 
-   dWxh = 
-   db =
-   dE
-
-
-
-class rnn():
-   def __init__(self, rnnDim, embeddingDim, sigmaSize, activation, weights):
+class RNN():
+   def __init__(self, rnnDim, embeddingDim, sigmaSize, weights):
       if weights:
          pass
       else:
-         self.Whh = torch.randn(rnnDim, rnnDim)
-         self.Wxh = torch.randn(rnnDim, embeddingDim)
-         self.b = torch.randn(rnnDim)
-         self.V = torch.randn(sigmaSize-1, rnnDim)
-         self.c = torch.zeros(sigmaSize-1)
-         self.E = torch.randn(embeddingDim, sigmaSize)
+         self.Whh = torch.randn(rnnDim, rnnDim) / np.sqrt(rnnDim)
+         self.Wxh = torch.randn(rnnDim, embeddingDim) / np.sqrt(embeddingDim)
+         self.b = torch.zeros(rnnDim)
+         self.V = torch.randn(sigmaSize-1, rnnDim) / np.sqrt(rnnDim)
+         self.c = torch.zeros(sigmaSize-1) 
+         self.E = torch.randn(embeddingDim, sigmaSize) / np.sqrt(sigmaSize)
+         self.dim = rnnDim
 
-   def forward(self, xt):
-      xt = self.E[:, xt]
-      at = torch.matmul(self.Whh, ht) + torch.matmul(self.Wxh, xt) + self.b
-      ht = torch.tanh(at)
-      ot = torch.matmul(self.V, ht) + self.c
-      yt = torch.exp(ot - torch.max(ot))/ torch.sum(ot - torch.max(ot))
-      return yt 
+   def softmax(self, x):
+      xnew = torch.exp(x - torch.max(x))
+      return xnew/torch.sum(xnew)
 
-   def backprop(self, delta):
-      
-      return gradients
-
-   def fit(self, x, y, lr, epochs):
-      for epoch in epochs:
-         for wi, wj in zip(x, y):
-            y_hat = self.forward(wi)
-            delta = y_hat[wj] - 1
-            gradients = self.backprop()
+   def fit(self, bigrams, lr, epochs):
+      Loss = []
+      loss = []
+      for epoch in range(epochs):
+         for sentence in bigrams:
+            # Separar los bigramas en 2 listas
+            x = [bigram[0] for bigram in sentence]
+            y = [bigram[1] for bigram in sentence]
+            # Lista del tiempo 
+            t = range(1, len(sentence) + 1)
+            # Inicializar el estado oculto en ceros
+            ht = torch.zeros(len(sentence)+1, self.dim)
+            for wi, wj, t in zip(x, y, t):
+               # Forward
+               xt = self.E[:, wi]
+               #print(xt)
+               at = torch.matmul(self.Whh, ht[t-1]) + torch.matmul(self.Wxh, xt) + self.b
+               ht[t] = torch.tanh(at)
+               ot = torch.matmul(self.V, ht[t]) + self.c
+               yt = self.softmax(ot)
+               # Error
+               dt = yt - 1
+               # Loss 
+               loss.append(-torch.log(yt[wj]+0.001))        
+               # Backprop
+               gradL_a = torch.matmul(torch.diag(1-torch.tanh(at)**2), torch.matmul(torch.t(self.V), dt))
+               dV = torch.ger(dt, ht[t])
+               dc = dt
+               dWhh = torch.ger(gradL_a, ht[t-1])
+               dWxh =torch.ger(gradL_a, xt) 
+               db = gradL_a
+               dE =  torch.matmul(torch.t(self.Wxh), gradL_a)
+               # Gradient descent
+               self.V -= lr*dV
+               self.c -= lr*dc
+               self.Whh -= lr*dWhh
+               self.Wxh -= lr*dWxh
+               self.b -= lr*db
+               self.E[:, wi] -= lr*dE
+         Loss.append(np.sum(loss))
+         print('Loss E{} = {}'.format(epoch+1, np.sum(loss)))
+         loss.clear()
 
    
-   
+# rnn = RNN(100, 100, len(Sigma), None)
+
+rnn = RNN(100, 300, len(Sigma), None)
+
+x = [bigram[0] for bigram in bigrams]
+y = [bigram[1] for bigram in bigrams]
+
+print('Sigma size = ', len(Sigma))
+print('\n\n')
+
+
+rnn.fit(bigrams, 0.007, 100)
 
 #torch.matmul(Whh, ht) + torch.matmul(Wxh, x)
 
